5efce8bc3e31ae5ba53335d0a169e805de6241f6
lian kip <simdigliandi@gmail.com>
2022-11-13 03:21:33 +0700

    Solved registrasi

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import DashboardView from '../views/DashboardView.vue'
 
 import LoginView from '../views/LoginView.vue';
+import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: DashboardView
-    },
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'login',
+            component: LoginView
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: DashboardView
+        },
+    ]
 })
 
 export default router
