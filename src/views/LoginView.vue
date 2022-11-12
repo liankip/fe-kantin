05176b9b095b7600d81c7e843825c6232b358216
lian kip <simdigliandi@gmail.com>
2022-11-12 16:37:04 +0700

    Push all code

---
+++ src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form @submit.prevent="userLogin" @keyup.enter="submit">
+      <input type="text" name="email" v-model="form.username">
+      <input type="password" name="password" v-model="form.password">
+      <button type="submit">Masuk</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    userLogin () {
+      axios.post('http://localhost:3000/users/login', this.form).then(Res => {
+        // this.$router.push({name: 'dashboard'})
+        console.log(Res)
+      }).catch(Err => {
+        console.log(Err.response.data.error)
+      })
+    }
+  }
+}
+</script>
